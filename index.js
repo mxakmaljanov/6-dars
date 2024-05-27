dcf5255f5db1e2e1e53698c152db952355791e6d
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-27 18:23:37 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,108 @@
+//1-masala
+//let n = +prompt("sonni kiriting");
+//for(let i=1;i<=n;i++){
+//    console.log(i)
+//}
+//2-masala
+//let n = +prompt("sonni kiriting");
+//for(let i = n;i>=1;i--){
+//    console.log(i);
+//}
+//3-masala
+//let n = +prompt("sonni kiriting");
+//let sum=0;
+//for(let i =1;i<=n;i++){
+//    sum=sum+i;
+//    console.log(sum);
+//}
+//4-masala
+//let n = +prompt("sonni kiriting");
+//let sum=0
+//for(let i=1;i<=n;i++){
+//    if(i%2==0){
+//        sum=sum+i
+//    }
+//
+//}
+//console.log(sum);
+//5-masala
+//let n = +prompt("sonni kiriting");
+//let sum=0
+//for(let i=1;i<=n;i++){
+//    if(i%2==1){
+//        sum=sum+i
+//    }
+//}
+//console.log(sum);
+// 6-misol
+// let a = +prompt("a sonni kiriting");
+// for(let i=1;i<=a;i++){
+    // if(a%i==0){
+        // console.log(i);
+   // }
+//}
+// 7-misol
+//let a = +prompt("a sonni kriting");
+//let sum=0;
+//let counter=0;
+//for(let i = 1;i<=a;i++);{
+//    if(a%i==0){
+//        sum=sum+i;
+//        counter=counter+1;
+//    }
+//}
+//console.log(sum);
+//console.log(counter);
+//9-misol
+//let a = +prompt("a sonni kiriting");
+//let counter=0;
+//for(let i = 1;i<=a;i++);{
+//    if(i%a==0){
+//        counter=counter+1
+//    }
+//    if(counter>2){
+//        console.log("murakkab son");
+//    }else if(counter=2){
+//         console.log("tub son");
+//    }
+//}
+//10-misol
+//let n = +prompt("sonni kiriting");
+//let k = +prompt("sonni kiriting");
+//for(let i = 1;1<=n;i++){
+//console.log(k);
+//}
+//11misol
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+// for(let i = a;i<=b;i++){
+    // console.log(i);
+//}
+//12-misol
+//let a = +prompt("a sonini kiriting")
+//let b = +prompt("b sonini kiriting")
+//for(let i=b-1; i>=a+1;i--){
+//        console.log(i);
+//}
+//
+//13-masala
+//let a=+prompt("sonni kiriting")
+//let b=+prompt("sonni kiriting")
+//for(  let i=1 a<=b; a++){
+//    console.log(a,b)
+//}
+//14-misol
+//let k=5
+//for(let i=+prompt("sonni kiting"); k<=i;++){
+//    console.log(k)
+//}
+//9-misol
+//let k=7
+//for(let i=+prompt("sonni kiting"); k<=i;k/10){
+//    console.log(k)
+//}
+//15-misol
+//let k=6
+//or(let i=+prompt("sonni kiting"); k<=i;k+0.1){
+//   console.log(k)}
+//
